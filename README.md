12362365e7364df48caee61680e66d22e0d6312a
Luan Martins <luan.cbm@hotmail.com>
2021-11-01 16:26:54 -0300

    first estructure

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>API petshop</h1>
+<h2>Cadastro de fornecedores</h2>
---
+++ api/rotas/fornecedores/index.js
@@ -0,0 +1,57 @@
+const roteador = require('express').Router()
+const TabelaFornecedor = require('./TabelaFornecedor')
+const Fornecedor = require('./Fornecedor')
+
+roteador.get('/', async (req, res) => {
+    const resultados = await TabelaFornecedor.listar()
+    res.status(200).send(resultados)
+})
+
+roteador.post('/', async (req, res) => {
+    try {
+        const dadosRecebidos = req.body
+        const fornecedor = new Fornecedor(dadosRecebidos)
+        await fornecedor.criar()
+        res.status(201).send(fornecedor)
+    } catch (erro) {
+        res.status(400).send({ mensagem: erro.message })
+    }
+})
+
+roteador.get('/:idFornecedor', async (req, res) => {
+    try {
+        const id = req.params.idFornecedor
+        const fornecedor = new Fornecedor({ id: id })
+        await fornecedor.carregar()
+        res.status(200).send(fornecedor)
+    } catch (erro) {
+        res.status(404).send({ mensagem: erro.message })
+    }
+})
+
+roteador.put('/:idFornecedor', async (req, res) => {
+    try {
+        const id = req.params.idFornecedor
+        const dadosRecebidos = req.body
+        const dados = Object.assign({}, dadosRecebidos, { id: id })
+        const fornecedor = new Fornecedor(dados)
+        await fornecedor.atualizar()
+        res.status(204).end()
+    } catch (erro) {
+        res.status(400).send({ mensagem: erro.message })
+    }
+})
+
+roteador.delete('/:idFornecedor', async (req, res) => {
+    try {
+        const id = req.params.idFornecedor
+        const fornecedor = new Fornecedor({ id: id })
+        await fornecedor.carregar()
+        await fornecedor.remover()
+        res.status(204).end()
+    } catch (erro) {
+        res.status(404).send({ mensagem: erro.message })
+    }
+})
+
+module.exports = roteador
