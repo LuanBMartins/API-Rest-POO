cccaf48be42732aaf79118f3d0adbcd1f83c9348
Luan Martins <luan.cbm@hotmail.com>
2021-11-03 13:52:16 -0300

    add middleware of validate Accept

--- api/index.js
+++ api/index.js
@@ -4,12 +4,28 @@ const config = require('config')
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+    const accept = ['application/json', '*/*']
+    const formato = req.header('Accept')
+    if(!accept.includes(formato)){
+        res.status(406).end()
+    }else{
+        const content_type = (formato === '*/*') ? 'application/json' : formato
+        res.setHeader('Content-Type', content_type)
+        next()
+    }
+
+})
+
 const roteador = require('./rotas/fornecedores')
 app.use('/api/fornecedores', roteador)
 
 app.use((erro, req, res, next) => {
     if(erro.message === 'Fornecedor não encontrado') {
         res.status(404).send({ mensagem: erro.message })    
+    }
+    else if(erro.message === 'Formato não suportado!') {
+        res.status(406).send({ message: erro.message })
     }
     res.status(400).send({ mensagem: erro.message })
 })
