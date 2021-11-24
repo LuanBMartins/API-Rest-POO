6c2cf22974c8ec05ef94d0f6d041e0fa28eb27ee
Luan Martins <luan.cbm@hotmail.com>
2021-11-24 09:54:30 -0300

    att devDependencies

--- api/index.js
+++ api/index.js
@@ -1,38 +1,38 @@
 const express = require('express')
 const app = express()
 const config = require('config')
-const {SerializadorError} = require('./utils/serializador')
+const { SerializadorError } = require('./utils/serializador')
 
 app.use(express.json())
 
 app.use((req, res, next) => {
-    const accept = ['application/json', '*/*', 'application/xml']
-    const formato = req.header('Accept')
-    if(!accept.includes(formato)){
-        res.status(406).end()
-    }else{
-        const content_type = (formato === '*/*') ? 'application/json' : formato
-        res.setHeader('Content-Type', content_type)
-        next()
-    }
+  const accept = ['application/json', '*/*', 'application/xml']
+  const formato = req.header('Accept')
+  if (!accept.includes(formato)) {
+    res.status(406).end()
+  } else {
+    const content = (formato === '*/*') ? 'application/json' : formato
+    res.setHeader('Content-Type', content)
+    next()
+  }
 })
 
 app.use((req, res, next) => {
-    res.set('Access-Control-Allow-Origin', '*')
-    next()
+  res.set('Access-Control-Allow-Origin', '*')
+  next()
 })
 
 const roteador = require('./rotas/fornecedores')
 app.use('/api/fornecedores', roteador)
 
 app.use((erro, req, res, next) => {
-    const serializador = new SerializadorError(res.getHeader('Content-type'))
-    console.log(erro);
-    if(erro.message === 'Fornecedor não encontrado' || erro.message === 'Produto não encontrado!') {
-        res.status(404).send(serializador.serializar(erro))    
-    }else{
-        res.status(500).send(serializador.serializar(erro))
-    }
+  const serializador = new SerializadorError(res.getHeader('Content-type'))
+  console.log(erro)
+  if (erro.message === 'Fornecedor não encontrado' || erro.message === 'Produto não encontrado!') {
+    res.status(404).send(serializador.serializar(erro))
+  } else {
+    res.status(500).send(serializador.serializar(erro))
+  }
 })
 
 app.listen(config.get('api.porta'), () => console.log('A API está funcionando!'))
--- api/utils/serializador.js
+++ api/utils/serializador.js
@@ -1,88 +1,86 @@
 const jsontoxml = require('jsontoxml')
 
 class Serializador {
-
-    serializar (dados) {
-        dados = this.filtrar(dados)
-        if (this.contentType === 'application/json') {
-            return this.json(dados)
-        }
-
-        if (this.contentType === 'application/xml') {
-            return this.xml(dados)
-        }
-  
-        throw new Error(`Content-type:${this.contentType}`)
+  serializar (dados) {
+    dados = this.filtrar(dados)
+    if (this.contentType === 'application/json') {
+      return this.json(dados)
     }
 
-    json (dados) {
-        return JSON.stringify(dados)
+    if (this.contentType === 'application/xml') {
+      return this.xml(dados)
     }
 
-    xml (dados) {
-        let tag = this.tagSingular
-
-        if(Array.isArray(dados)) {
-            tag = this.tagPlurar  
-            dados = dados.map(item => {
-                return {
-                    [this.tagSingular]: item
-                }
-            })
-        }  
-
-        return jsontoxml({[tag]: dados})
-    }
-  
-  
-    filtrarObjeto (dados) {
-        const novoObjeto = {}
-        this.camposPublicos.forEach((campo) => {
-            if (dados.hasOwnProperty(campo)) {
-                novoObjeto[campo] = dados[campo]
-            }
-        })
-        return novoObjeto
-    }
-  
-    filtrar (dados) {
-        if (Array.isArray(dados)) {
-            dados = dados.map(item => {
-                return this.filtrarObjeto(item)
-            })
-        } else {
-            dados = this.filtrarObjeto(dados)
-        }
-        return dados
-    }
+    throw new Error(`Content-type:${this.contentType}`)
   }
 
-class SerializadorFornecedor extends Serializador {
-    constructor (contentType, campos_extras) {
-        super()
-        this.contentType = contentType
-        this.camposPublicos = [
-            'id',
-            'empresa',
-            'categoria',
-            ...campos_extras || []
-        ]
-        this.tagSingular = 'fornecedor'
-        this.tagPlurar = 'fornecedores'
+  json (dados) {
+    return JSON.stringify(dados)
+  }
+
+  xml (dados) {
+    let tag = this.tagSingular
+
+    if (Array.isArray(dados)) {
+      tag = this.tagPlurar
+      dados = dados.map(item => {
+        return {
+          [this.tagSingular]: item
+        }
+      })
     }
+
+    return jsontoxml({ [tag]: dados })
+  }
+
+  filtrarObjeto (dados) {
+    const novoObjeto = {}
+    this.camposPublicos.forEach((campo) => {
+      if (Reflect.has(dados, campo)) {
+        novoObjeto[campo] = dados[campo]
+      }
+    })
+    return novoObjeto
+  }
+
+  filtrar (dados) {
+    if (Array.isArray(dados)) {
+      dados = dados.map(item => {
+        return this.filtrarObjeto(item)
+      })
+    } else {
+      dados = this.filtrarObjeto(dados)
+    }
+    return dados
+  }
 }
 
-class SerializadorError extends  Serializador {
-    constructor(contentType, campos_extras) {
-        super()
-        this.contentType = contentType
-        this.camposPublicos = ['message', ...campos_extras || []]
-        this.tagSingular = 'erro'
-        this.tagPlurar = 'erros'
-    }
+class SerializadorFornecedor extends Serializador {
+  constructor (contentType, camposextras) {
+    super()
+    this.contentType = contentType
+    this.camposPublicos = [
+      'id',
+      'empresa',
+      'categoria',
+      ...camposextras || []
+    ]
+    this.tagSingular = 'fornecedor'
+    this.tagPlurar = 'fornecedores'
+  }
+}
+
+class SerializadorError extends Serializador {
+  constructor (contentType, camposextras) {
+    super()
+    this.contentType = contentType
+    this.camposPublicos = ['message', ...camposextras || []]
+    this.tagSingular = 'erro'
+    this.tagPlurar = 'erros'
+  }
 }
 
 module.exports = {
-    SerializadorFornecedor,
-    SerializadorError
+  SerializadorFornecedor,
+  SerializadorError
 }
